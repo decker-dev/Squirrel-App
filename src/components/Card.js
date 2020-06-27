cbfe6f3e60bca786c88bb6c2f8a1e54083c21141
Decker <deckerurbano@gmail.com>
2020-06-27 19:00:08 -0300

    state

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,19 @@
 import React from 'react'
 const red ="#fff"
 class Card extends React.Component {
+    constructor(props){
+        super(props)
+        this.state={
+            image:'https://avatars0.githubusercontent.com/u/59459088?s=460&u=b7742094c605876db8a05faa733efa3e71b40ea4&v=4'
+        }
+    }
+    componentDidMount(){
+        setTimeout(()=>{
+            this.setState({
+                image:'https://github.githubassets.com/images/icons/emoji/shipit.png'
+            })
+        },200)
+    }
     render() {
         const { title, img, description } = this.props
         return (
@@ -11,7 +24,7 @@ class Card extends React.Component {
                 <div className="card-body">
                     <div className="row center">
                         <div className="mx-auto">
-                            <img src={img} />
+                            <img src={this.state.image} />
                         </div>
                         <div className="mx-auto row ">
                             <h1> {title} </h1>
