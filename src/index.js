32f4eb5c9dae70453ac0887d09c48c03aa5e5bed
Decker <deckerurbano@gmail.com>
2020-06-27 16:32:11 -0300

    JSX Docs

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const name = 'Pedro'
+const user = {
+    firstName: 'Decker',
+    LastName: 'Urbano',
+    avatar: 'https://lh3.googleusercontent.com/proxy/rQtaiFPZDSC8sP85JPxK6Qf6_oLj5gfWnAzRCRIvOQG22qVjd8WAGjINwqo89l-KBmWa3hI6Jb4eVA'
+}
+function getname(userf) {
+    return `${userf.firstName} ${userf.LastName}`
+}
+function getGreeting(userf) {
+    if (userf) {
+        return (
+            <div>
+                <h1> getname(userf)</h1>
+                <img src={userf.avatar} />
+            </div>
+        );
+    }
+    return <h1>Hello, Stranger.</h1>;
+}
+
+const element = getGreeting(user);
+const container = document.getElementById('root')
+
+ReactDom.render(element, container)
